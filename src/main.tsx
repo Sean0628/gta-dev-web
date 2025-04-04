6a23790fe25cc5df47e98f18b2f970b4c1ab3d37
Sho Ito <i.sho.0628@gmail.com>
2025-04-04 00:40:31 -0400

    chore: use the one for vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from './App.tsx';
 import './index.css';
+
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
-    <SpeedInsights />
   </StrictMode>
 );
