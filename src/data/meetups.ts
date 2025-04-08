b2eb167e6c7c33cc4806d25ca275f19b575446c6
tinaba96 <tinaba96.ca@gmail.com>
2025-04-08 00:20:11 -0400

    [Taka] add meetups

--- src/data/meetups.ts
+++ src/data/meetups.ts
@@ -1,68 +1,148 @@
 export interface MeetupSource {
   url: string;
-  platform: 'meetup' | 'other';
-  type: 'meetups' | 'others';
+  platform: "meetup" | "other";
+  type: "meetups" | "others";
 }
 
 export const meetupSources: MeetupSource[] = [
   {
     url: "https://www.meetup.com/techtank-to/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/torontojs/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/go-toronto/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/toronto-java-users-group/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/k8s-ca/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/drupalto/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/toronto-aws-users-united/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/elastic-toronto-user-group/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/toronto-tech-stack-exchange/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://www.meetup.com/machine-learning-to-meetup/",
     platform: "meetup",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://toronto-ruby.com/",
     platform: "other",
-    type: "meetups"
+    type: "meetups",
   },
   {
     url: "https://builder-sundays.myshopify.com/",
     platform: "other",
-    type: "others"
-  }
+    type: "others",
+  },
+  {
+    url: "https://www.meetup.com/brainstation-toronto-tech-skills-and-careers/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/metro-toronto-azure-community/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/pragmatic-tech/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/laravel-toronto/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/vue-toronto/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/toronto-modern-data/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/cloud-architecture-meetup-group/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/aittg-toronto/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/digitalnatives/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.techinto.ca/",
+    platform: "other",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/mindstone-toronto-ai-meetup/", // "https://community.mindstone.com/events"
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://www.genaicollective.ai/chapters/toronto",
+    platform: "other",
+    type: "meetups",
+  },
+  {
+    url: "https://aitinkerers.org/p/welcome",
+    platform: "other",
+    type: "meetups",
+  },
+  {
+    url: "https://www.meetup.com/toronto-ai-aligners/",
+    platform: "meetup",
+    type: "meetups",
+  },
+  {
+    url: "https://taico.ca",
+    platform: "other",
+    type: "meetups",
+  },
+  {
+    url: "https://www.techto.org",
+    platform: "other",
+    type: "meetups",
+  },
 ];
